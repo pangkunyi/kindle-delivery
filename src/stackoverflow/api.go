53974ee0389d395556201610233c2167983cfe37
pangkunyi <pangkunyi@gmail.com>
2013-06-17 16:08:57 +0800

    retreive answers

--- src/stackoverflow/api.go
+++ src/stackoverflow/api.go
@@ -1,3 +1,4 @@
+/* vim: set ts=4 sw=2 enc=utf-8: */
 package stackoverflow
 
 import(
@@ -9,6 +10,7 @@ import(
 
 const (
 	questions_api_url=`https://api.stackexchange.com/2.1/questions/%s?order=desc&sort=activity&site=stackoverflow&filter=!T5TVQmNC(GmExNmJa6`
+	answer_api_url=`https://api.stackexchange.com/2.1/answers/%v?order=desc&sort=activity&site=stackoverflow&filter=!1zKuht46WZuyLgV0nAfmB`
 )
 
 type Questions struct {
@@ -22,12 +24,31 @@ type Question struct {
 	Accepted_answer_id int64
 }
 
+type Answers struct {
+	Items []Answer
+}
+
+type Answer struct {
+	Question_id int64
+	Body string
+	Answer_id int64
+}
+
 func question(q *Questions, id string) (error){
-	content, err := httpRequest(fmt.Sprintf(questions_api_url, id))
+	return jsonApi(q, fmt.Sprintf(questions_api_url, id))
+}
+
+func answer(a *Answers, id int64) (error){
+	return jsonApi(a, fmt.Sprintf(answer_api_url, id))
+}
+
+func jsonApi(v interface{}, apiUrl string) (error){
+	content, err := httpRequest(apiUrl)
 	if err!=nil{
 		return err
 	}
-	err =json.Unmarshal(content, q)
+
+	err =json.Unmarshal(content, v)
 	if err!=nil{
 		return err
 	}
--- src/stackoverflow/atom.go
+++ src/stackoverflow/atom.go
@@ -1,8 +1,11 @@
+/* vim: set ts=4 sw=2 enc=utf-8: */
 package stackoverflow
 
 import(
 	feeder "github.com/jteeuwen/go-pkg-rss"
 	"strings"
+	"io/ioutil"
+	"os"
 	"fmt"
 )
 
@@ -10,19 +13,49 @@ var items []*feeder.Item
 var url ="http://stackexchange.com/feeds/tagsets/88069/golang?sort=active"
 var feed =feeder.New(0, false, chanHandler, itemHandler)
 var idPrefixLen=len("http://stackoverflow.com/questions/")
+var template=`
+<html>
+<head>
+	<title>stackoverflow</title>
+	<style>
+		pre{
+			 background-color: #eeeeee;
+		}
+	</style>
+</head>
+<body>
+%s
+</body>
+</html>
+`
+var questionTemplate=`<H1>%s</H1>%s<br/><H2>Answer</H2>%s<br/>`
 func chanHandler(feed *feeder.Feed, newchannels []*feeder.Channel) {
 	//println(len(newchannels), "new channel(s) in", feed.Url)
 }
 
 func itemHandler(feed *feeder.Feed, ch *feeder.Channel, newitems []*feeder.Item) {
 	items = newitems
-
 	println(len(newitems), "new item(s) in", ch.Title, "of", feed.Url)
+	var content string
 	for _,item := range items{
 		id := getItemQuestionId(item)
 		var qs Questions
 		question(&qs, id)
-		fmt.Printf("title:%s\nbody:%s\n", qs.Items[0].Title, qs.Items[0].Body)
+		if qs.Items[0].Accepted_answer_id > 0 {
+		  var ans Answers
+		  answer(&ans, qs.Items[0].Accepted_answer_id)
+		  content=content+fmt.Sprintf(questionTemplate, qs.Items[0].Title, qs.Items[0].Body, ans.Items[0].Body)
+		  //fmt.Printf("title:%s\nbody:%s\nanswer:%s\n----------------------------", qs.Items[0].Title, qs.Items[0].Body, ans.Items[0].Body)
+		}
+	}
+	write(content)
+}
+
+func write(content string){
+	err := ioutil.WriteFile(os.Getenv("HOME")+"/stackoverflow.html", []byte(fmt.Sprintf(template,content)), os.ModePerm)
+	if err != nil {
+		fmt.Printf("fail to write file, cause by: %v\n", err)
+		return
 	}
 }
 
