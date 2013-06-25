c544bf083258a9c6906dcf9c2f3e0f39c40e0ebe
pangkunyi <pangkunyi@gmail.com>
2013-06-25 15:14:56 +0800

    update

--- src/kindle-delivery/main.go
+++ src/kindle-delivery/main.go
@@ -1,7 +1,6 @@
 package main
 
 import(
-	sof "stackoverflow"
 	"mail"
 	"os"
 	"fmt"
@@ -9,6 +8,8 @@ import(
 	"io/ioutil"
 	"bytes"
 	"net/http"
+	"errors"
+	sof "stackoverflow"
 )
 func main(){
 	http.HandleFunc("/send2kindle", send2kindleHandler)
@@ -18,35 +19,40 @@ func main(){
 	}
 }
 func send2kindleHandler(w http.ResponseWriter, r *http.Request){
-	send2kindle()
+	err := send2kindle()
+	if err != nil {
+		fmt.Fprintf(w,"error:%s", err.Error())
+	}
 	fmt.Fprintf(w,"success")
 }
-func send2kindle(){
+func send2kindle() error{
 	filename :="/tmp/stackoverflow.html"
 	err:= sof.Update()
 	if err!=nil{
-		panic(err)
+		return err
 	}
+	panic(errors.New("test error"))
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
-	    fmt.Printf("no stackoverflow html file create: %s", filename)
-	    panic(err)
+		fmt.Printf("no stackoverflow html file create: %s", filename)
+		return err
 	}
 	kindlegen()
 	filename ="/tmp/stackoverflow.mobi"
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
-	    fmt.Printf("no stackoverflow mobi file create: %s", filename)
-	    panic(err)
+		fmt.Printf("no stackoverflow mobi file create: %s", filename)
+		return err
 	}
-	
+
 	body, err :=ioutil.ReadFile(filename)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	err = mail.Send(body, "stackoverflow", ".mobi")
 	if err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
 
 func kindlegen(){
